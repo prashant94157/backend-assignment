ebe2847b3f2b61593d5e1e73ea8e7c4dbbc1d187
prashant94157 <0166621m@gmail.com>
2023-09-10 15:47:46 +0530

    initiated backend using nodejs

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
+
+import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middlewares/errorMiddlewares.js';
+
+// import {errorHandler, notFound}
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
